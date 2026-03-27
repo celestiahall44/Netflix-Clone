2317213033143d5272daffa350842fc6cba0ef6a
celestiahall44 <celestia.hall44@gmail.com>
2026-03-27 08:38:17 -0600

    friday

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,29 +1,44 @@
-import React, {useEffect, useRef } from 'react'
+import React, {useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom';
 
 const TitleCards = ({title, category}) => {
 
-const cardsRef = useRef();
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMDcwYjcyZjZhZTIwYzMxN2M3NzU5YmJiNmZmMmE1YyIsIm5iZiI6MTc3NDYxNzI2NS4zODYsInN1YiI6IjY5YzY4MmIxNmIzZjIwOGFlY2FmZDNjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._mTzDZSlwOpqMN0LwP71IXovMdwGEwN9JfcTeKVz5Lg'
+    }
+  };
 
 const handleWheel = (event)=>{
   event.preventDefault();
   cardsRef.current.scrollLeft += event.deltaY;
 } 
 
-useEffect(() => {
-  cardsRef.current.addEventListener('wheel', handleWheel);
-},[])
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : 'now_playing'}`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
+    cardsRef.current.addEventListener('wheel', handleWheel);
+  },[])
 
   return (
     <div className='title-cards'>
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) =>{
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card, index) =>{
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
         })}
       </div>
     </div>
